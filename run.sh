b59fd6642adafe2863a9339da5e35520ceebb320
George Araújo <george.gcac@gmail.com>
2024-11-26 18:19:16 -0300

    Commented other codes for debugging
    Signed-off-by: George Araújo <george.gcac@gmail.com>

--- run.sh
+++ run.sh
@@ -8,19 +8,19 @@ if hash hyperfine 2>/dev/null; then
     echo "Running $n_runs times the $(basename $PWD) benchmark"
     hyperfine \
         --setup 'bash ../compile.sh' --cleanup 'bash ../clean.sh' --runs $n_runs --show-output --export-markdown "$(basename $PWD).md" \
-        'bun ./js/code.js 40' \
-        './c/code 40' \
-        './dart/code 40' \
-        'deno ./js/code.js 40' \
-        './go/code 40' \
-        'java jvm.code 40' \
-        'java -jar kotlin/code.jar 40' \
-        'node ./js/code.js 40' \
-        'php ./php/code.php 40' \
-        'python3 ./py/code.py 40' \
-        'Rscript ./r/code.R 40' \
-        'ruby ./ruby/code.rb 40' \
-        './rust/target/release/code 40'
+        'bun ./js/code.js 40'
+        # './c/code 40' \
+        # './dart/code 40' \
+        # 'deno ./js/code.js 40' \
+        # './go/code 40' \
+        # 'java jvm.code 40' \
+        # 'java -jar kotlin/code.jar 40' \
+        # 'node ./js/code.js 40' \
+        # 'php ./php/code.php 40' \
+        # 'python3 ./py/code.py 40' \
+        # 'Rscript ./r/code.R 40' \
+        # 'ruby ./ruby/code.rb 40' \
+        # './rust/target/release/code 40'
 else
     echo "hyperfine is not installed. Please refer to https://github.com/sharkdp/hyperfine for installation instructions."
 fi

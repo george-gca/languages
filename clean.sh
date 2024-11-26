722d624c84dd5143727033611b805f4a100d0785
George Araújo <george.gcac@gmail.com>
2024-11-26 15:54:41 -0300

    Commented clean code
    Signed-off-by: George Araújo <george.gcac@gmail.com>

--- clean.sh
+++ clean.sh
@@ -1,6 +1,6 @@
-rm c/code
-rm go/code
-rm jvm/code.class
-rm -r rust/target
-rm -rf kotlin/code.jar
-rm dart/code
+# rm c/code
+# rm go/code
+# rm jvm/code.class
+# rm -r rust/target
+# rm -rf kotlin/code.jar
+# rm dart/code

54d31c5bc6192637bfbbdab3f1346b69eed2104d
George Araújo <george.gcac@gmail.com>
2024-11-26 18:43:46 -0300

    Comment rust code
    Signed-off-by: George Araújo <george.gcac@gmail.com>

--- run.sh
+++ run.sh
@@ -8,6 +8,7 @@ if hash hyperfine 2>/dev/null; then
     echo "Running $n_runs times the $(basename $PWD) benchmark"
     hyperfine \
         --setup 'bash ../compile.sh' --cleanup 'bash ../clean.sh' --runs $n_runs --show-output --export-markdown "$(basename $PWD).md" \
+        'bun ./js/code.js 40' \
         './c/code 40' \
         './dart/code 40' \
         'deno ./js/code.js 40' \
@@ -18,9 +19,8 @@ if hash hyperfine 2>/dev/null; then
         'php ./php/code.php 40' \
         'python3 ./py/code.py 40' \
         'Rscript ./r/code.R 40' \
-        'ruby ./ruby/code.rb 40' \
-        './rust/target/release/code 40'
-        # 'bun ./js/code.js 40' \ # throwing error when executing: toolchain 'nightly-x86_64-unknown-linux-gnu' is not installed
+        'ruby ./ruby/code.rb 40'
+        # './rust/target/release/code 40' # throwing error when executing: toolchain 'nightly-x86_64-unknown-linux-gnu' is not installed
 else
     echo "hyperfine is not installed. Please refer to https://github.com/sharkdp/hyperfine for installation instructions."
 fi
